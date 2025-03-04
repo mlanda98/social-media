f142c4c4142e99f1cbe1c687d08d3ee692481e65
Mayra Landa <mlandae16@gmail.com>
2025-03-04 14:28:44 -0800

    import routes to index in backend

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const passport = require("../backend/passportConfig");
 const authRoutes = require("./routes/authRoutes");
 const followRoutes = require("./routes/followRoutes");
+const postRoutes = require("./routes/postRoutes");
+const profileRoutes = require("./routes/profileRoutes");
 
 require("dotenv").config();
 const app = express();
@@ -10,7 +12,9 @@ app.use(express.json());
 app.use(passport.initialize());
 
 app.use("/auth", authRoutes);
-app.use("/api", followRoutes)
+app.use("/follow", followRoutes);
+app.use("/post", postRoutes);
+app.use("/profile", profileRoutes);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on portal ${PORT}`));
--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -80,3 +80,5 @@ router.patch("/users/profile", authenticateJWT, async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
+
+module.exports = router;
