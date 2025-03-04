9979061804c82c0617a2c9a8a929f77c6d318136
Mayra Landa <mlandae16@gmail.com>
2025-03-04 13:49:08 -0800

    add route to make profile and update

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const prisma = require("../prismaClient");
+
+const router = express.Router();
+
+const jwt = require("jsonwebtoken");
+const { use } = require("passport");
+
+const authenticateJWT = (req, res, next) => {
+  const token = req.header("Authorization");
+
+  if (!token) {
+    return res.status(401).json({ message: "Access denied. No token" });
+  }
+
+  try {
+    const decoded = jwt.verify(token.split(" ")[1], process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(403).json({ message: "Invalid or expired token" });
+  }
+};
+
+router.get("users/:username", async (req, res) => {
+  try {
+    const { username } = req.params;
+    const user = await prisma.user.findUnique({
+      where: { username },
+      include: {
+        posts: {
+          orderBy: { createdAt: "desc" },
+          include: {
+            likes: true,
+            comments: {
+              include: { user: { select: { username } } },
+            },
+          },
+        },
+      },
+    });
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    res.json({
+      id: user.id,
+      username: user.username,
+      avatar:
+        user.avatar ||
+        `https://wwww.gravatar.com/avatar/${md5(user.email)}?d=identicon`,
+      posts: user.posts.map((post) => ({
+        id: post.id,
+        content: post.content,
+        likesCount: post.comments.map((comment) => ({
+          id: comment.id,
+          content: comment.content,
+          author: comment.user.username,
+        })),
+        createdAt: post.createdAt,
+      })),
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.patch("/users/profile", authenticateJWT, async (req, res) => {
+  try {
+    const userId = req.user.userId;
+    const { username, avatar } = req.body;
+
+    const updatedUser = await prisma.user.update({
+      where: { id: userId },
+      data: {
+        username,
+        avatar,
+      },
+    });
+    res.json({ message: "Profile updated", updatedUser });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
