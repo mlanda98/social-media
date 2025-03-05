f7276aadfd5782faa10c3d29399556d7cabe6ffc
Mayra Landa <mlandae16@gmail.com>
2025-03-04 19:06:52 -0800

    add register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import SignIn from "./pages/SignIn";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,57 @@
+import { use, useState } from "react";
+
+const Register = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail]= useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("http://localhost:8000/auth/register", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({username, email, password})
+    })
+    
+    const data = await response.json();
+    if (response.ok)
+      {
+        alert("Registration successful! Please log in.");
+  } else {
+    alert(data.message);
+  }
+}
+
+return (
+  <div>
+    <h2>Register</h2>
+    <form onSubmit={handleSubmit}>
+    <input
+    type="text"
+    placeholder="Username"
+    value={username}
+    onChange={(e) => setUsername(e.target.value)}
+    required
+    />
+    <input
+    type="email"
+    placeholder="Email"
+    value={email}
+    onChange={(e) => setEmail(e.target.value)}
+    required
+    />
+    <input
+    type="password"
+    placeholder="Password"
+    value={password}
+    onChange={(e) => setPassword(e.target.value)}
+    required
+    />
+ <button type="submit">Register</button>
+    </form>
+  </div>
+)
+}
+
+export default Register;
