07b67eee26484b2041b07a5c636fb2daa6bebeb7
Mayra Landa <mlandae16@gmail.com>
2025-03-04 18:33:16 -0800

    add sign in page

---
+++ frontend/src/pages/SignIn.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("http://localhost:8000/auth/login", {
+      method: "POST", 
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({email, password}),
+    })
+
+    const data = await response.json();
+    if (response.ok){
+      localStorage.setItem("token", data.token)
+      alert("Login successful!");
+    } else {
+      alert(data.message);
+    }
+  }
+
+  return (
+    <div>
+      <h2>Sign In</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+        />
+        <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  )
+}
+
+export default SignIn;
